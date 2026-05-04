1c858e1a99a4a2084c754e944724ffb0f9004407
aishhiro <trieg13s@gmail.com>
2026-05-04 17:00:19 +0300

    Refactor argument parsing and error handling

--- sh/5.sh
+++ sh/5.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# разбор ключей
 while getopts "e:w:h" opt; do
     case $opt in
         e) exec 2>>"$OPTARG" ;;
@@ -11,11 +10,9 @@ while getopts "e:w:h" opt; do
 done
 shift $((OPTIND - 1))
 
-# проверки
 [[ $# -eq 0 ]] && echo " не указаны каталоги" >&2 && exit 1
 [[ -z "$min" || ! "$min" =~ ^[0-9]+$ ]] && echo "укажите число слов через -w" >&2 && exit 1
 
-# поиск файлов с колво слов > min
 for dir in "$@"; do
     [[ -d "$dir" ]] || { echo ": $dir не каталог" >&2; continue; }
     find "$dir" -type f | while read -r f; do
