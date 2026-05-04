99e6291e41b47fca7941ab41731fbca4e2581225
aishhiro <trieg13s@gmail.com>
2026-05-04 16:57:38 +0300

    Update 1.sh

--- sh/1.sh
+++ sh/1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#аргументы
 while getopts "e:h" opt; do
     case $opt in
         e) exec 2>>"$OPTARG" ;;  # перенаправляние stderr 
@@ -9,12 +8,10 @@ while getopts "e:h" opt; do
     esac
 done
 
-# таблица
 printf "%-8s" "N"
 for e in {0..6}; do printf "%-12s" "N^$e"; done
 echo
 
-# цифры
 for n in {1..9}; do
     printf "%-8d" $n
     r=1
