be873ae5217e8040f414e9ba92db12e2b881d1cf
aishhiro <trieg13s@gmail.com>
2026-05-04 16:59:55 +0300

    Update 4.sh

--- sh/4.sh
+++ sh/4.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# разбор ключей
 while getopts "e:hd:" opt; do
     case $opt in
         e) exec 2>>"$OPTARG" ;;
@@ -11,14 +10,12 @@ while getopts "e:hd:" opt; do
 done
 shift $((OPTIND - 1))
 
-# проверка что имя файла указано
 [[ $# -eq 0 ]] && echo "ошибка: не указано имя файла" >&2 && exit 1
 
 filename="$1"
+
 dir="${dir:-.}"
 
-# проверка существования каталога
 [[ -d "$dir" ]] || { echo "ошибка: $dir не каталог" >&2; exit 1; }
 
-# поиск каталога с указ названинем
 find "$dir" -type f -name "$filename" -printf '%h\n' 2>/dev/null | sort -u
