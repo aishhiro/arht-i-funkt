118a3d778d34e5d60c6157bbe61b8a15799ee588
aishhiro <trieg13s@gmail.com>
2026-05-14 04:47:03 +0300

    Update 2.c

--- c/2.c
+++ c/2.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    printf("введиче число");
+    int num, i, iprime = 1; 
+    if (scanf("%d", &num) != 1 || num <= 0) 
+    {
+        printf("ошибка. вы ввели не число или число <0\n");
+    }
+        else
+    {
+        if(num == 1)
+        {
+            printf("\n");
+        }
+        else
+        {
+            for(i = 2; i <= (int)sqrt(num); i++) //sqrt корень к целому
+           {
+                if(num % i == 0)
+                {
+                    iprime = 0;
+                    break;
+                }
 
+            }
+            if (iprime == 0)               
+            {
+                printf("%d составное\n", num);
+            }
+            else
+            {
+                printf("%d простое\n", num);
+            }
+        }
+    }    
+    return 0;  
+}
